62b27f77dadd19eec3d337fbd3059c70aef25325
Joey-Michael Fallone <jm@sp0d3rm0b1le.local>
2018-02-03 20:52:38 -0700

    Finished tutorial4

---
+++ tutorial4/kivy_language.py
@@ -0,0 +1,15 @@
+from kivy.app import App 
+from kivy.uix.scatter import Scatter
+from kivy.uix.boxlayout import BoxLayout
+
+class ScatterTextWidget(BoxLayout):
+	pass
+
+class TutorialApp(App):
+	def build(self):
+		return ScatterTextWidget()
+
+		
+
+if __name__ == "__main__":
+	TutorialApp().run()
